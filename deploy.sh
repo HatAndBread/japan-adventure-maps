fb9e71f8927443ea420b44f53c5fccaaf021bd5e
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2022-05-08 17:11:38 +0900

    Deploy Sun May  8 17:11:38 JST 2022

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@ echo "Are you sure? y/n"
 read ANSWER
 
 if [ "$ANSWER" = "y" ]; then
+  echo "Running tests... 🧪"
+  TEST_RESULT=$(bundle exec rspec | grep "0 failures")
+  FAILURE_NUMBER=${#VAR}
+  echo $FAILURE_NUMBER
+  if $FAILURE_NUMBER -ne 2; then
+    exit
+  fi
   echo "Building Assets 🔨"
   RAILS_ENV=production bundle exec rake assets:precompile
   echo "Pushing to git 🕺 "
